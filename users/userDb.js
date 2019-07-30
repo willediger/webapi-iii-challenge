28646d030cf5c02322be2a0bcd98a06e9ecdd5de
unknown <will.ediger@gmail.com>
2019-07-30 18:01:53 -0500

    modified update user to return new user

--- users/userDb.js
+++ users/userDb.js
@@ -1,4 +1,4 @@
-const db = require('../data/dbConfig.js');
+const db = require("../data/dbConfig.js");
 
 module.exports = {
   get,
@@ -6,28 +6,28 @@ module.exports = {
   getUserPosts,
   insert,
   update,
-  remove,
+  remove
 };
 
 function get() {
-  return db('users');
+  return db("users");
 }
 
 function getById(id) {
-  return db('users')
+  return db("users")
     .where({ id })
     .first();
 }
 
 function getUserPosts(userId) {
-  return db('posts as p')
-    .join('users as u', 'u.id', 'p.user_id')
-    .select('p.id', 'p.text', 'u.name as postedBy')
-    .where('p.user_id', userId);
+  return db("posts as p")
+    .join("users as u", "u.id", "p.user_id")
+    .select("p.id", "p.text", "u.name as postedBy")
+    .where("p.user_id", userId);
 }
 
 function insert(user) {
-  return db('users')
+  return db("users")
     .insert(user)
     .then(ids => {
       return getById(ids[0]);
@@ -35,13 +35,16 @@ function insert(user) {
 }
 
 function update(id, changes) {
-  return db('users')
+  return db("users")
     .where({ id })
-    .update(changes);
+    .update(changes)
+    .then(() => {
+      return getById(id);
+    });
 }
 
 function remove(id) {
-  return db('users')
-    .where('id', id)
+  return db("users")
+    .where("id", id)
     .del();
 }
