549838e265d277f9eca50c03b757beb95e98d98e
unknown <will.ediger@gmail.com>
2019-07-30 16:47:21 -0500

    added routers for users and posts

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 
+const userRouter = require("./users/userRouter.js");
+const postRouter = require("./posts/postRouter.js");
+
 const server = express();
 
 server.use(express.json());
 server.use(logger);
+
+server.use("/api/users", userRouter);
+server.use("/api/posts", postRouter);
 
 server.get("/", (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
