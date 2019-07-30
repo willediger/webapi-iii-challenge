f7a31462385f5136db2dd6e6d1168f7a25f5efa7
unknown <will.ediger@gmail.com>
2019-07-30 16:49:16 -0500

    built validateUserId, get(/:id), and error handler; passed custom functions as middleware to routes appropriately

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,56 @@
-const express = 'express';
+const express = "express";
 
 const router = express.Router();
+const db = require("./userDb.js");
 
-router.post('/', (req, res) => {
+router.post("/", validateUser, (req, res) => {});
 
+router.post("/:id/posts", validateUserId, validatePost, (req, res) => {});
+
+router.get("/", (req, res) => {});
+
+router.get("/:id", validateUserId, (req, res) => {
+  res.send(200).json(req.user);
 });
 
-router.post('/:id/posts', (req, res) => {
+router.get("/:id/posts", validateUserId, (req, res) => {});
 
-});
+router.delete("/:id", validateUserId, (req, res) => {});
 
-router.get('/', (req, res) => {
+router.put("/:id", validateUserId, (req, res) => {});
 
-});
+//error handler
+router.use((err, req, res, next) => {
+  console.error(err);
 
-router.get('/:id', (req, res) => {
-
-});
-
-router.get('/:id/posts', (req, res) => {
-
-});
-
-router.delete('/:id', (req, res) => {
-
-});
-
-router.put('/:id', (req, res) => {
-
+  res.status(err.status).json({ message: err.message });
 });
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+async function validateUserId(req, res, next) {
+  try {
+    const { id } = req.params;
+    const user = await db.getById(id);
+    if (user) {
+      req.user = user;
+      next();
+    } else {
+      next({
+        status: 404,
+        message: "The user with the specified ID does not exist."
+      });
+    }
+  } catch {
+    next({
+      status: 500,
+      message: "The user information could not be retrieved."
+    });
+  }
+}
 
-};
+async function validateUser(req, res, next) {}
 
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
+async function validatePost(req, res, next) {}
 
 module.exports = router;
